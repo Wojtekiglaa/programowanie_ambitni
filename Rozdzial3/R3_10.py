2d67afb1c4f8cdda096b48bd9b9e697de6d3c83b
Wojciech Igielski <wojtekiglaa@gmail.com>
2023-12-26 23:09:50 +0100

    ołkej lets go

--- Rozdzial3/R3_10.py
+++ Rozdzial3/R3_10.py
@@ -1,1 +1,13 @@
-
+a = int(input("Podaj x: "))
+c = 0
+arr = []
+for y in range(1,a+1):
+    for x in range(1,a+1):
+        if(a%x == 0):
+            c+=1
+        if(c == 2):
+            arr.append(x)
+        c = 0
+# zobacze czy to tym mozna
+print(arr)
+#aha jednak nie
--- Rozdzial3/R3_9.py
+++ Rozdzial3/R3_9.py
@@ -1,6 +1,15 @@
 a = int(input("Podaj x: "))
-if(a%a == 0 and a%1 == 0):
+c = 0
+for x in range(1,a+1):
+    if(a%x == 0):
+        c+=1
+if(c == 2):
     print("Liczba jest pierwsza")
-#petla trzeba
-# ide spac
-#todo: zrobic okok
+else:
+    print("Liczba nie jest pierwsza")
+#if(a%a == 0 and a%1 == 0):
+    #print("Liczba jest pierwsza")
+#ej to ma sens chyba xd
+#jestem bogiem
+#https://www.matemaks.pl/liczby-pierwsze.html
+#liczba calkowita nieujemna to tez mamy o to sprawdzac?? idk
