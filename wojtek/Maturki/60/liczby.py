8fd77a9dbf99ae3b606cd16071ebf8988644239d
Wojciech Igielski <wojtekiglaa@gmail.com>
2024-02-04 12:56:41 +0100

    boze

---
+++ wojtek/Maturki/60/liczby.py
@@ -0,0 +1,37 @@
+import math
+liczby = open("liczby.txt", "r").readlines()
+liczby = [x.strip() for x in liczby]
+count = 0
+for x in liczby:
+    if int(x) < 1000:
+        count += 1
+print(count)
+#--------------------------------------------------
+ostatnie = []
+for x in liczby:
+    ostatnie.append(int(x)) #????
+print(sorted(ostatnie)[-2:])
+#--------------------------------------------------
+#znajdz liczbe dzielnikow
+def dzielniki(n):
+    dzielniki = []
+    for i in range(1, n+1):
+        if n % i == 0:
+            dzielniki.append(i)
+    return [len(dzielniki), dzielniki]
+for x in liczby:
+    x = int(x)
+    if dzielniki(x)[0] == 18:
+        print(f"{x}: {sorted(dzielniki(x)[1])}")
+#--------------------------------------------------
+liczby = [int(x.strip()) for x in liczby]
+def gcd(a, b):
+    return math.gcd(a, b)
+# Iterate over the numbers in descending order
+for x in sorted(liczby, reverse=True):
+    # Check if x is relatively prime to all other numbers
+    if all(gcd(x, y) == 1 for y in liczby if y != x):
+        # If it is, print it and break the loop
+        print(x)
+        break
+#colpilot to napisal kurwa co sie odpuerdala
---
+++ wojtek/Maturki/61/ciagi.py
@@ -0,0 +1,20 @@
+#dobra to jest w miare es
+ciagi = open("ciagi.txt", "r").readlines()
+ciagi = [x.split() for x in ciagi]
+bledne = open("bledne.txt", "r").readlines()
+bledne = [x.split() for x in bledne]
+# jakos trzeba tego import zrobic
+sameciagi = []
+for x in ciagi:
+    if len(x) == 1:
+        continue
+    else:
+        sameciagi.append(x)
+#---------------------
+count = 0
+for x in sameciagi:
+    roznica = int(x[1]) - int(x[0])
+
+    if int(x[1]) - int(x[0]) != int(x[2]) - int(x[1]):
+        count += 1
+print(count)
