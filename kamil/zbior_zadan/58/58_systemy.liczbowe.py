be7c82cc5ae7ae0f49427f5bbe02a8b89da76ffb
kamil <kzawieja2006@gmail.com>
2024-02-05 18:13:18 +0100

    z lapka

--- kamil/zbior_zadan/58/58_systemy.liczbowe.py
+++ kamil/zbior_zadan/58/58_systemy.liczbowe.py
@@ -8,6 +8,16 @@ stacja3 = open("dane_systemy3.txt", "r").readlines()
 stacja3 = [a.strip().split(' ') for a in stacja3]
 
 # 58.1 ----------------------------
-
-
+temp1,temp2,temp3 = [],[],[]
+godz1,godz2,godz3 = [],[],[]
+for x in stacja1:
+    temp1.append(int(x[1], 2))
+    godz1.append(int(x[0], 2))
+for r in stacja1:
+    temp2.append(int(x[1], 4))
+    godz2.append(int(x[0], 4))
+for a in stacja1:
+    temp3.append(int(x[1], 8))
+    godz3.append(int(x[0], 8))
+print(min(temp1),min(temp2),min(temp3))
 
