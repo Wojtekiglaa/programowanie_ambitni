41664dc28f9326f9bd2570d5511f7e48afdca679
Kamil_dom <kzawieja2006@gmail.com>
2024-02-06 17:30:07 +0100

    g

---
+++ kamil/Matury/2017/roz/2017_piksele.py
@@ -0,0 +1,4 @@
+lines = open("dane.txt","r").readlines()
+lines = [x.strip().split(' ') for x in lines]
+
+# 6.1 --------------------------------
---
+++ kamil/zbior_zadan/69/69_geny.py
@@ -0,0 +1,5 @@
+lines = open("dane_geny.txt","r").readlines()
+lines = [x.strip() for x in lines]
+
+# 69.1 ---------------------------
+def geninlines(a):
