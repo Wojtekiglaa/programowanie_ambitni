781aad94ea6af44b670c0deac5e456d64979e88d
kamil <kzawieja2006@gmail.com>
2024-04-16 14:56:06 +0200

    z lapka

--- kamil/Matury/2017/roz/2017_piksele_samemu.py
+++ kamil/Matury/2017/roz/2017_piksele_samemu.py
@@ -1,7 +1,8 @@
-lines = open("dane.txt","r").readlines()
+lines = open('dane.txt','r').readlines()
 lines = [x.strip().split() for x in lines]
 lines = [[int(y) for y in x] for x in lines]
-# 6.1 --------------------------------
+
+#6.1 -------------------------------
 # x = []
 # y = []
 # for x in lines:
@@ -9,28 +10,23 @@ lines = [[int(y) for y in x] for x in lines]
 #     y.append(min(x))
 # print(max(x),min(y))
 
-# 6.2 ----------------------------------
+
+#6.2 ---------------------------------
 # ile = 0
 # for x in range(200):
 #     if lines[x] != lines[x][::-1]:
 #         ile += 1
 # print(ile)
 
-# 6.3 -----------------------------------
-
-
-
-
-
-# 6.4 -----------------------------------
+#6.4 ---------------------------------
 max = 0
-ile = 1
-for y in range(0,320):
+for y in range(0, 320):
+    ile = 1
     for x in range(1,200):
-        if lines[x][y] == lines[x-1][y]:
-            ile += 1
+        if lines[x-1][y] == lines[x][y]:
+            ile + 1
         else:
-            if ile>max:
+            if ile > max:
                 max = ile
             ile = 1
 print(max)
--- kamil/zbior_zadan/74/74_hasla.py
+++ kamil/zbior_zadan/74/74_hasla.py
@@ -1,27 +1,75 @@
-hasla = open("hasla.txt", "r").readlines()
-hasla = [x.split() for x in hasla]
+# hasla = open("hasla.txt", "r").readlines()
+# hasla = [x.split() for x in hasla]
+#
+# #74.1 ------------------------------------
+# # num = 0
+# # for x in hasla:
+# #     for y in x:
+# #         if y.isdigit():
+# #             num += 1
+# # print(num)
+#
+# # # 74.2 -------------------------------------
+# # litery = []
+# # a = []
+# # for x in hasla:
+# #     if hasla.count(x) > 1:
+# #         litery.append(x)
+# # for i in litery:
+# #     if i not in a:
+# #         a.append(i)
+# # a.sort()
+# # print(a)
+#
+# # 74.3 ------------------------------------------
+# # ile = 0
+# # male = []
+# # duze = []
+#
+#
+# def Fib(n):
+#     a = 1
+#     b = 1
+#     for i in range(n):
+#         b = b + a
+#         a = b - a
+#     return b
+# n = int(input('podaj dlugosc ciagu: '))
+# print(Fib(n))
+# def Fibo(n):
+#     a , b = 1, 1
+#     for i in range(n - 1):
+#         b += a
+#         a = b - a
+#     return a
+# n = int(input("Podaj nr liczby z ciągu Fibonacciego: "))
+#
+# print(Fibo(n))
 
-#74.1 ------------------------------------
-# num = 0
-# for x in hasla:
-#     for y in x:
-#         if y.isdigit():
-#             num += 1
-# print(num)
+# tab = [5, 3, 8, 2, 1, 7]
+# n = len(tab)
+# for i in range(n - 1):
+#     for j in range(n - 1):
+#         if tab[j] > tab[j + 1]:
+#             zmienna = tab[j]
+#             tab[j] = tab[j + 1]
+#             tab[j + 1] = zmienna
+# for x in tab:
+#     print(x, end=" ")
 
-# # 74.2 -------------------------------------
-# litery = []
-# a = []
-# for x in hasla:
-#     if hasla.count(x) > 1:
-#         litery.append(x)
-# for i in litery:
-#     if i not in a:
-#         a.append(i)
-# a.sort()
-# print(a)
+def prime(x):
+    for n in range(2, x):
+        if x%n==0:
+            return False
+        return True
+def jd(x):
+    num = []
+    for n in range(2, x):
+        if (n%2 != 0 and prime(n)):
+            num.append(n)
+    for a in num:
+        for b in num:
+            if a+b == x:
+                return a,b
+print(jd(20))
 
-# 74.3 ------------------------------------------
-ile = 0
-male = []
-duze = []
