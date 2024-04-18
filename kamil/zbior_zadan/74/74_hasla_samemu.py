0fe2afdb959cacf0fd5e273ea2e58bd533763d16
Kamil_dom <kzawieja2006@gmail.com>
2024-04-18 17:18:49 +0200

    g

---
+++ kamil/zbior_zadan/74/74_hasla_samemu.py
@@ -0,0 +1,30 @@
+hasla = open('hasla.txt','r').readlines()
+hasla = [x.split() for x in hasla]
+
+# 74.1 --------------------
+# num = 0
+# for x in hasla:
+#     for y in x:
+#         if y.isdigit():
+#             num += 1
+# print(num)
+
+# 74.2 --------------------
+# litery = []
+# a = []
+# for x in hasla:
+#     if hasla.count(x) > 1:
+#         litery.append(x)
+# for i in litery:
+#     if i not in a:
+#         a.append(i)
+# a.sort()
+# print(a)
+
+# 74.4
+ile = 0
+for x in hasla:
+    for y in x:
+        if y.upper():
+            ile +=1
+print(y)
